fedefdc5d639f9e578a32232e34027f9f1d3d5ab
rohitmane417 <88490692+rohitmane417@users.noreply.github.com>
2024-10-15 13:40:50 +0530

    Add files via upload

---
+++ Rohit Mane/RelationalOperator.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x=10;
+		int y=20;
+		boolean res1=x>=y;
+		boolean res2=x<=y;
+		boolean res3=x==y;
+		boolean res4=x!=y;
+		boolean res5=x>y;
+		boolean res6=x<y;
+	
+		
+       System.out.println("x is greater than equal to y : "+res1);
+       System.out.println("x is less than equal to y : " +res2);
+       System.out.println("x is equal to y : "+ res3);
+       System.out.println("x is not equal to y : "+ res4);
+       System.out.println("x is greater than y :" +res5);
+       System.out.println("x is less y" +res6);
+      
+
+	}
+
+}
